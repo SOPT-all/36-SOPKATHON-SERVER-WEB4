b56245953a8baf5d530eaf0f75b4ca0b7dc18e39
bricksky <skydh1214@naver.com>
2025-05-18 01:59:40 +0900

    feat: database 연결

---
+++ src/main/java/org/sopkathon/web4/sopkathon36serverweb4/domain/user/UserController.java
@@ -0,0 +1,10 @@
+package org.sopkathon.web4.sopkathon36serverweb4.domain.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/login")
+public class UserController {
+
+}
---
+++ src/main/java/org/sopkathon/web4/sopkathon36serverweb4/domain/user/UserService.java
@@ -0,0 +1,4 @@
+package org.sopkathon.web4.sopkathon36serverweb4.domain.user;
+
+public interface UserService {
+}
