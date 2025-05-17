a584fd736aadb193dc35ad94d193c34271dcf88f
bingle625 <fhqhxm8899@gmail.com>
2025-05-17 15:41:54 +0900

    chore: 로깅인터셉터 loggingUtil 사용하도록 수정

--- src/main/java/org/sopkathon/web4/sopkathon36serverweb4/global/interceptor/LoggingInterceptor.java
+++ src/main/java/org/sopkathon/web4/sopkathon36serverweb4/global/interceptor/LoggingInterceptor.java
@@ -2,17 +2,25 @@ package org.sopkathon.web4.sopkathon36serverweb4.global.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.sopkathon.web4.sopkathon36serverweb4.global.util.LoggingUtil;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 public class LoggingInterceptor implements HandlerInterceptor {
+
+  private final LoggingUtil loggingUtil;
+
+  public LoggingInterceptor(final LoggingUtil loggingUtil) {
+    this.loggingUtil = loggingUtil;
+  }
+
   @Override
   public boolean preHandle(
       HttpServletRequest request,
       HttpServletResponse response,
       Object handler) throws Exception {
-    System.out.println("[LOG] " + request.getMethod() + " " + request.getRequestURI());
+    loggingUtil.info("[LOG] " + request.getMethod() + " " + request.getRequestURI());
     return true;
   }
 
@@ -21,6 +29,6 @@ public class LoggingInterceptor implements HandlerInterceptor {
       HttpServletRequest request,
       HttpServletResponse response,
       Object handler, Exception ex) throws Exception {
-    System.out.println("[LOG] Response Status: " + response.getStatus());
+    loggingUtil.info("[LOG] Response Status: " + response.getStatus());
   }
 }
