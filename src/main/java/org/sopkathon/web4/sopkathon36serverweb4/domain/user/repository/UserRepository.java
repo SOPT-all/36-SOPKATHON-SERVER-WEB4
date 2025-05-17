5268c3122dc55a8070e334b5bbf54d85c6c03ef9
bricksky <skydh1214@naver.com>
2025-05-18 04:01:58 +0900

    feat: post 로직 검증

--- src/main/java/org/sopkathon/web4/sopkathon36serverweb4/domain/user/repository/UserRepository.java
+++ src/main/java/org/sopkathon/web4/sopkathon36serverweb4/domain/user/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package org.sopkathon.web4.sopkathon36serverweb4.domain.user.repository;
 
+import org.sopkathon.web4.sopkathon36serverweb4.domain.user.dto.UserSignupRequestDto;
 import org.sopkathon.web4.sopkathon36serverweb4.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByName(String name);
 }
--- src/main/java/org/sopkathon/web4/sopkathon36serverweb4/domain/user/service/UserServiceImpl.java
+++ src/main/java/org/sopkathon/web4/sopkathon36serverweb4/domain/user/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package org.sopkathon.web4.sopkathon36serverweb4.domain.user.service;
 import lombok.RequiredArgsConstructor;
 import org.sopkathon.web4.sopkathon36serverweb4.domain.user.dto.UserSignupRequestDto;
 import org.sopkathon.web4.sopkathon36serverweb4.domain.user.dto.UserSignupResponseDto;
+import org.sopkathon.web4.sopkathon36serverweb4.domain.user.entity.User;
 import org.sopkathon.web4.sopkathon36serverweb4.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,7 +15,12 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
-    public UserSignupResponseDto findByName(UserSignupRequestDto userSignupRequestDto){
-        return new UserSignupResponseDto("test");
+    public UserSignupResponseDto findByName(UserSignupRequestDto userSignupRequestDto) {
+        User user = userRepository
+                .findByName(userSignupRequestDto.name())
+                .orElseThrow(() -> new IllegalArgumentException(
+                        "User not found with name: " + userSignupRequestDto.name()
+                ));
+        return new UserSignupResponseDto(user.getName());
     }
 }
